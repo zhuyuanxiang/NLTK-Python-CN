169b1faf938072a0fb445c3dee5a298b86a7d4e7
zYx.Tom <zhuyuanxiang@gmail.com>
2020-11-30 19:00:05 +0800

    C0801~C0804.Over

--- PythonNLP/C08.py
+++ PythonNLP/C08.py
@@ -1,20 +1,36 @@
-# Ch8 分析句子结构
-
+# -*- encoding: utf-8 -*-
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   NLTK-Python-CN
+@File       :   C08.py
+@Version    :   v0.1
+@Time       :   2020-11-30 16:39
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :
+@Desc       :
+@理解：
+"""
 import nltk
 
+from tools import show_subtitle
+
+# Ch8 分析句子结构
 # 前面的章节重点关注词的处理：识别、分析结构、分配词汇类别、获取词汇的含义、识别词序列或者n-grams的模式
 # 本章的学习目标：
 # 1）使用形式化语法来描述无限的句子集合的结构
 # 2）使用句法树来表示句子结构
 # 3）使用解析器来分析句子，并且自动构建语法树
 
-# 1 一些语法困境
-# 1.1 语言数据和无限的可能性
+# 8.1 一些语法困境
+# 8.1.1 语言数据和无限的可能性
 # 本章中将采用“生成文法”的形式化框架，其中一种“语言”被认为仅仅是所有合乎文法的句子的大集合，
 # 而文法只是一个形式化符号，可以用于“生成”这个集合的成员。文法使用 “S → S and S” 形式的递归产生式
 
-# 1.2 普遍存在的歧义
-# 下面这个文法允许以两种方式分析句子
+# 8.1.2 普遍存在的歧义
 groucho_grammar = nltk.CFG.fromstring('''
 S -> NP VP
 PP -> P NP
@@ -26,21 +42,22 @@ V -> "shot"
 P -> "in"
 ''')
 
+# 基于一种文法解析句子，可能会解析出两种结构
 sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
 parser = nltk.ChartParser(groucho_grammar)  # 图解析
-for tree in parser.parse(sent):
+for i, tree in enumerate(parser.parse(sent)):
+    show_subtitle(f"第 {i + 1} 个结构")
     print(tree)
-tree.draw()
 
-# 2. 文法的用途
-# 2.1 超越 n-grams
+# 8.2 文法的用途
+# 8.2.1 超越 n-grams
 # 成分结构是词与词结合在一起组成的单元。
 # 在符合语法规则的句子中的词序列是可以被一个更小的词序列替代，并且这个词序列不会导致句子不合语法规则
 # 形成单元的每个序列都可以被单独的词替换。
 # 句子长度是任意的，因此短语结构树的深度也是任意的。因为Sec7.4只能产生有限深度的结构，所以分块方法并不适合用于句法分析。
 
-# 3. 上下文无关文法（context-free grammars，CFG）
-# 3.1 一种简单的文法
+# 8.3 上下文无关文法（context-free grammars，CFG）
+# 8.3.1 一种简单的文法
 
 # Ex8-1 一个简单的上下文无关文法的例子
 grammar1 = nltk.CFG.fromstring("""
@@ -57,24 +74,30 @@ P -> "in" | "on" | "by" | "with"
 # 句子剖析会出现两个符合文法规则的结果，称为结构上有歧义。这个歧义称为介词短语附着歧义。
 sent = 'The dog saw a man in the park'.split()
 rd_parser = nltk.RecursiveDescentParser(grammar1)  # 递归下降解析器 RecursiveDescentParser()
-for tree in rd_parser.parse(sent):
+for i, tree in enumerate(rd_parser.parse(sent)):
+    show_subtitle(f"第 {i + 1} 个结构")
     print(tree)
-tree.draw()
+
 nltk.app.rdparser()  # 通过这个演示可以辅助理解从顶向下的回溯策略的句法剖析过程
 
-# 3.2 编写自己的文法：在文本文件创建和编辑语法会更加文法，然后可以利用函数加载到NLTK中进行解析
+# 8.3.2 编写自己的文法
+# 在文本文件创建和编辑语法会更加文法，然后可以利用函数加载到NLTK中进行解析
 # grammar1 = nltk.data.load('file:D:/mygrammar1.cfg')
 grammar1 = nltk.data.load('mygrammar.cfg')
 sent = "Mary saw Bob".split()
 sent = "Mary saw Bob's mother".split()  # 无法解析的句子
 rd_parser = nltk.RecursiveDescentParser(grammar1)  # trace = 2 不知道有何作用
-for tree in rd_parser.parse(sent):
+for i, tree in enumerate(rd_parser.parse(sent)):
+    show_subtitle(f"第 {i + 1} 个结构")
     print(tree)
 
 for p in grammar1.productions():
     print(p)
 
-# 3.3. 句法结构中的递归
+# 8.3.3 句法结构中的递归
+# RecursiveDescentParser()无法处理形如X→XY的左递归产生式
+
+# Ex-2：递归的上下文无关文法
 grammar2 = nltk.CFG.fromstring("""
 S  -> NP VP
 NP -> Det Nom | PropN
@@ -91,13 +114,12 @@ P -> 'on'
 
 sent = 'the angry bear chased the frightened little squirrel'.split()
 sent = 'Chatterer said Buster thought the tree was tall'.split()
-# RecursiveDescentParser()无法处理形如X→XY的左递归产生式
 rd_parser = nltk.RecursiveDescentParser(grammar2)
-for tree in rd_parser.parse(sent):
+for i, tree in enumerate(rd_parser.parse(sent)):
+    show_subtitle(f"第 {i + 1} 个结构")
     print(tree)
-tree.draw()
 
-# 4. 上下文无关文法分析
+# 8.4 上下文无关文法分析
 # 解析器根据文法产生式处理输入的句子，并且建立一个或者多个符合文法的组成结构
 # 文法是一个格式良好的声明规则——实际上只是一个字符串，而不是程序。
 # 解析器是文法的解释程序，用于搜索所有的符合文法的树的空间，并且找出一棵与句子匹配的语法树
@@ -124,16 +146,17 @@ P -> "in" | "on" | "by" | "with"
 sent = 'Mary saw a dog'.split()
 sr_parser = nltk.ShiftReduceParser(grammar1)
 # sr_parser = nltk.ShiftReduceParser(grammar1,trace = 2)
-for tree in sr_parser.parse(sent):
+for i, tree in enumerate(rd_parser.parse(sent)):
+    show_subtitle(f"第 {i + 1} 个结构")
     print(tree)
-tree.draw()
+
 nltk.app.rdparser()  # 通过这个演示可以辅助理解自顶向下的回溯策略的句法剖析过程
 nltk.app.srparser()  # 通过这个演示可以辅助理解自底向上的称进归约的句法剖析过程
 
-# 4.3 左角落解析器：是 自顶向下 和 自底向上 方法的混合体，是一个带有自底向上过滤的自顶向下的解析器
+# 8.4.3 左角落解析器：是 自顶向下 和 自底向上 方法的混合体，是一个带有自底向上过滤的自顶向下的解析器
 # 左角落解析器，不会陷入左递归产生式死循环。
 
-# 4.4 符合语句规则的子串表（WFST）
+# 8.4.4 符合语句规则的子串表（WFST）
 # 上述简单的解析器都存在完整性和效率问题，下面将基于图表分析：即利用动态规划算法来解决这些问题
 # 动态规划算法存储中间结果，并且在适当的时候重用，从而显著提高了效率。
 # WFST的缺点：
@@ -145,7 +168,7 @@ nltk.app.srparser()  # 通过这个演示可以辅助理解自底向上的称进
 # 可以在文法中直接查找文本中单词所属类别
 # lhs : left-hand-side; rhs : right-hand-side
 text = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
-productions = groucho_grammar.productions(rhs = text[3])
+productions = groucho_grammar.productions(rhs=text[3])
 productions[0].lhs()
 
 
@@ -154,12 +177,12 @@ def init_wfst(tokens, grammar):
     numtokens = len(tokens)
     wfst = [[None for i in range(numtokens + 1)] for j in range(numtokens + 1)]
     for i in range(numtokens):
-        productions = grammar.productions(rhs = tokens[i])
+        productions = grammar.productions(rhs=tokens[i])
         wfst[i][i + 1] = productions[0].lhs()
     return wfst
 
 
-def complete_wfst(wfst, tokens, grammar, trace = False):
+def complete_wfst(wfst, tokens, grammar, trace=False):
     index = dict((p.rhs(), p.lhs()) for p in grammar.productions())
     numtokens = len(tokens)
     for span in range(2, numtokens + 1):
@@ -178,9 +201,9 @@ def complete_wfst(wfst, tokens, grammar, trace = False):
 def display(wfst, tokens):
     print('\nWFST ' + ' '.join([("%-4d" % i) for i in range(1, len(wfst))]))
     for i in range(len(wfst) - 1):
-        print("%d   " % i, end = " ")
+        print("%d   " % i, end=" ")
         for j in range(1, len(wfst)):
-            print("%-4s" % (wfst[i][j] or '.'), end = " ")
+            print("%-4s" % (wfst[i][j] or '.'), end=" ")
         print()
 
 
@@ -192,7 +215,7 @@ wfst1 = complete_wfst(wfst0, tokens, groucho_grammar)
 display(wfst1, tokens)
 
 # 显示剖析过程
-wfst1 = complete_wfst(wfst0, tokens, groucho_grammar, trace = True)
+wfst1 = complete_wfst(wfst0, tokens, groucho_grammar, trace=True)
 display(wfst1, tokens)
 
 nltk.app.chartparser()
